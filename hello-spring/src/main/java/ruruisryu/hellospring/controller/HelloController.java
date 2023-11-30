41caa897810e132cb8b1232a2d80a5cacc1e9282
ruruisryu <atz999@ewhain.net>
2023-11-30 23:50:30 +0900

    MVC와 템플릿 엔진

--- hello-spring/src/main/java/ruruisryu/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/ruruisryu/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package ruruisryu.hellospring.controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         // resources/templates 산하에 있는 hello.html파일을 찾아 렌더링을 한다.
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
