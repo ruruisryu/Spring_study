a508c5b424e249ca32b8018ff8c45d3baa36e483
ruruisryu <atz999@ewhain.net>
2023-12-01 00:38:20 +0900

    회원 도메인과 리포지토리 만들기

---
+++ hello-spring/src/main/java/ruruisryu/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package ruruisryu.hellospring.domain;
+
+public class Member {
+    public long id;
+    public String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ hello-spring/src/main/java/ruruisryu/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package ruruisryu.hellospring.repository;
+
+import ruruisryu.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        // 멤버의 Id를 정해준 뒤 맵에 저장해줌
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        // member 중 매개변수로 입력된 name과 같은 이름을 가진 member를 필터링해 반환
+        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        // store(맵)에 들어있던 value값들을 리스트 형태로 반환
+        return new ArrayList<>(store.values());
+    }
+}
